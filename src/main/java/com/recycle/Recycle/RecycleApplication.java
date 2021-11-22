a23c6411a20c764234dd2a8041a2961cd715f16e
Benilson <benilson.mtr@gmail.com>
2021-11-22 18:55:50 -0300

    added cors method

--- src/main/java/com/recycle/Recycle/RecycleApplication.java
+++ src/main/java/com/recycle/Recycle/RecycleApplication.java
@@ -2,7 +2,10 @@ package com.recycle.Recycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,4 +16,15 @@ public class RecycleApplication {
 		SpringApplication.run(RecycleApplication.class, args);
 	}
 
+}
+
+@Configuration
+class CorsConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
 }
